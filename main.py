b7b8053c020eac4d4fe804dad15b0eb75a9d8991
侯贤浩 <2023010842@secoder.net>
2025-06-24 18:02:17 +0800

    add gitignore

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def run_script_silently(script_name):
     subprocess.run(['python', script_name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
 
 if __name__ == "__main__":
-    # run_script_silently('./src/processor.py')
+    run_script_silently('./src/processor.py')
     run_script('./src/test.py')
